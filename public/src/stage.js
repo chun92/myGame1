c144e3550738fd4a11fc1c421c71c5d0c11c169e
Chun <chunuiyu@gmail.com>
2022-10-12 22:53:22 +0900

    test: energy를 map에 random하게 generate

--- public/src/stage.js
+++ public/src/stage.js
@@ -16,13 +16,49 @@ export class Stage {
 
     async test() {
         await this.initialize();
-        await this.addEnergyOnMap(0, 0, 0, EnergyType.ENERGY_BLUE);
+        //await this.addEnergyOnMap(0, 0, 0, EnergyType.ENERGY_BLUE);
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+        }
+
+        for (let pos in this.map.tileMap) {
+            let energyType = EnergyType.ENERGY_BLACK;
+            switch (getRandomInt(0, 7)) {
+                case 0:
+                    energyType = EnergyType.ENERGY_BLACK;
+                    break;
+                case 1:
+                    energyType = EnergyType.ENERGY_BLUE;
+                    break;
+                case 2:
+                    energyType = EnergyType.ENERGY_GREEN;
+                    break;
+                case 3:
+                    energyType = EnergyType.ENERGY_ORANGE;
+                    break;
+                case 4:
+                    energyType = EnergyType.ENERGY_RED;
+                    break;
+                case 5:
+                    energyType = EnergyType.ENERGY_WHITE;
+                    break;
+                case 6:
+                    energyType = EnergyType.ENERGY_YELLOW;
+                    break;
+            }
+            const tile = this.map.tileMap[pos];
+            const energy = new Energy(energyType, Vector2DFactory.make(0, 0), PositionBase.CENTER, 20, tile, this.scene);
+            await energy.initialize();
+            tile.setObject(energy);
+        }
     }
 
     async initialize() {
         // TODO: stage info will be saved as json or xml file format later
         const map = new Map(100, this.scene);
-        await map.initialize(2);
+        await map.initialize(3);
         this.scene.addChild(map);
         this.map = map;
     }
