8dca8d4d317578ecf1595146546b5ff538d57095
Chun <chunuiyu@gmail.com>
2022-11-19 19:38:57 +0900

    fix: layermanager의 setObject 잘몽되는 문제 해결

--- public/src/common/layerManager.js
+++ public/src/common/layerManager.js
@@ -6,11 +6,10 @@ export class LayerManager {
     initialize(stage) {
         stage.sortableChildren = true;
         this.group = Object.entries(LayerGroup).reduce((res, elem) => produce(res, draft => {
-            const key = elem[0];
             const value = elem[1];
             const group = new Group(value, true);
             stage.addChild(new Layer(group));
-            draft[key] = group;
+            draft[value] = group;
         }), {});
     }
 
